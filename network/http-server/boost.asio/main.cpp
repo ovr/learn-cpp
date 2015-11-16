6068482aec607405f0c6f6dd485ecbc278d1e09c
Dmitry Patsura <talk@dmtry.me>
2015-11-16 23:48:36 +0300

    Async timer

--- network/http-server/boost.asio/main.cpp
+++ network/http-server/boost.asio/main.cpp
@@ -4,14 +4,24 @@
 
 using namespace std;
 
+void print(const boost::system::error_code& /*e*/)
+{
+    cout << "Async timer" << endl;
+}
+
 int main() {
     cout << "Hello, World!" << endl;
 
     boost::asio::io_service io;
-    boost::asio::deadline_timer t(io, boost::posix_time::seconds(5));
-    t.wait();
 
-    cout << "Sync timer, 5sec was expired!" << endl;
+    boost::asio::deadline_timer asyncTimer(io, boost::posix_time::seconds(5));
+    asyncTimer.async_wait(print);
+
+//    boost::asio::deadline_timer syncTimer(io, boost::posix_time::seconds(5));
+//    syncTimer.wait();
+//    cout << "Sync timer, 5sec was expired!" << endl;
+
+    io.run();
 
     return 0;
 }
