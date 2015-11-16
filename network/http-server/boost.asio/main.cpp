730884b5e75d8990e79294986967f131913a3ff9
Dmitry Patsura <talk@dmtry.me>
2015-11-16 23:45:15 +0300

    Async timer

--- network/http-server/boost.asio/main.cpp
+++ network/http-server/boost.asio/main.cpp
@@ -7,7 +7,11 @@ using namespace std;
 int main() {
     cout << "Hello, World!" << endl;
 
+    boost::asio::io_service io;
+    boost::asio::deadline_timer t(io, boost::posix_time::seconds(5));
+    t.wait();
 
+    cout << "Sync timer, 5sec was expired!" << endl;
 
     return 0;
 }
