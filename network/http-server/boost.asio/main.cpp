5be09685d5d29e0d01dfcf19f2057e4932366a4a
Dmitry Patsura <talk@dmtry.me>
2015-11-17 00:00:07 +0300

    Implement async timer with restart after await end

--- network/http-server/boost.asio/main.cpp
+++ network/http-server/boost.asio/main.cpp
@@ -1,12 +1,25 @@
 #include <iostream>
 #include <string>
 #include <boost/asio.hpp>
+#include <boost/bind.hpp>
 
 using namespace std;
 
-void print(const boost::system::error_code& /*e*/)
+void print(const boost::system::error_code& /*e*/,
+           boost::asio::deadline_timer* t, int* count)
 {
-    cout << "Async timer" << endl;
+    cout << "Async timer " << *count << endl;
+    ++(*count);
+
+    /**
+     * Restart timer
+     */
+    t->expires_at(t->expires_at() + boost::posix_time::seconds(1));
+    t->async_wait(
+            boost::bind(
+                    print, boost::asio::placeholders::error, t, count
+            )
+    );
 }
 
 int main() {
@@ -14,8 +27,18 @@ int main() {
 
     boost::asio::io_service io;
 
-    boost::asio::deadline_timer asyncTimer(io, boost::posix_time::seconds(5));
-    asyncTimer.async_wait(print);
+    boost::asio::deadline_timer asyncTimer(io, boost::posix_time::seconds(1));
+
+    int count = 0;
+    asyncTimer.async_wait(
+            /**
+             * http://www.boost.org/doc/libs/1_58_0/libs/bind/doc/html/bind.html
+             */
+            boost::bind(
+                    print,
+                    boost::asio::placeholders::error, &asyncTimer, &count
+            )
+    );
 
 //    boost::asio::deadline_timer syncTimer(io, boost::posix_time::seconds(5));
 //    syncTimer.wait();
