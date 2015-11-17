ab624521359e68c56c26ce1990e7475598d2dbaa
Dmitry Patsura <talk@dmtry.me>
2015-11-17 22:09:17 +0300

    Convert propert_tree to User object in heap

--- json-parse/main.cpp
+++ json-parse/main.cpp
@@ -4,6 +4,12 @@
 
 using namespace std;
 
+class User {
+public:
+    long id;
+    string name;
+};
+
 int main() {
     string inputJSON = "{\"id\":567,\"name\":\"Removed\",\"age\":null,\"about\":null,\"avatar_id\":null,\"avatar\":{\"id\":null,\"url\":\"http:\\/\\/cdn.blabla.eu\\/img\\/sad_smile.png\",\"width\":null,\"height\":null,\"fill_color\":null,\"parent_id\":null},\"gender\":\"F\",\"friends_count\":1,\"subscribers_count\":0,\"interests_count\":0,\"birth_year\":null,\"birth_month\":null,\"birth_day\":null,\"country\":null,\"region\":null,\"city\":null,\"map_image\":null,\"relation\":null,\"is_service\":false,\"last_act\":\"2015-08-19 10:41:28.473371\",\"is_online\":false}";
 
@@ -13,7 +19,15 @@ int main() {
     boost::property_tree::ptree pt;
     boost::property_tree::read_json(ss, pt);
 
-    std::cout << pt.get<std::string>("id") << std::endl;
+    auto *user = new User();
+    user->id = pt.get<long>("id");
+    user->name = pt.get<string>("name");
+
+    std::cout << "User: " << endl
+            << user->id << endl
+            << user->name << endl;
+
+    delete user;
 
     return 0;
 }
