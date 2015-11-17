9612b3a62e05bcfe24b73dce929c8882fc05d810
Dmitry Patsura <talk@dmtry.me>
2015-11-18 00:14:44 +0300

    Store users in map<long, User>

--- json-parse/main.cpp
+++ json-parse/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <boost/property_tree/ptree.hpp>
 #include <boost/property_tree/json_parser.hpp>
+#include <map>
 
 using namespace std;
 
@@ -11,9 +12,8 @@ public:
     int age = 0;
 };
 
-int main() {
-    string inputJSON = "{\"id\":567,\"name\":\"Removed\",\"age\":null,\"about\":null,\"avatar_id\":null,\"avatar\":{\"id\":null,\"url\":\"http:\\/\\/cdn.blabla.eu\\/img\\/sad_smile.png\",\"width\":null,\"height\":null,\"fill_color\":null,\"parent_id\":null},\"gender\":\"F\",\"friends_count\":1,\"subscribers_count\":0,\"interests_count\":0,\"birth_year\":null,\"birth_month\":null,\"birth_day\":null,\"country\":null,\"region\":null,\"city\":null,\"map_image\":null,\"relation\":null,\"is_service\":false,\"last_act\":\"2015-08-19 10:41:28.473371\",\"is_online\":false}";
-
+void parseUser(string inputJSON, map<long, User> *users)
+{
     std::stringstream ss;
     ss << inputJSON;
 
@@ -24,18 +24,34 @@ int main() {
     user->id = pt.get<long>("id");
     user->name = pt.get<string>("name");
 
-
     auto tmp = (pt.get<string>("age"));
-    if (tmp == "null") {
-        cout << "Age is null" << endl;
-    } else {
+    if (tmp != "null") {
         user->age = atoi(tmp.c_str());
     }
 
-    std::cout << "User: " << endl
-            << user->id << endl
-            << user->name << endl
-            << user->age << endl;
+    users->insert(pair<long, User>(user->id, *user));
+}
+
+int main() {
+    auto *users = new map<long, User>();
+
+    string inputJSON;
+
+    inputJSON = "{\"id\":567,\"name\":\"Removed\",\"age\":null,\"about\":null,\"avatar_id\":null,\"avatar\":{\"id\":null,\"url\":\"http:\\/\\/cdn.blabla.eu\\/img\\/sad_smile.png\",\"width\":null,\"height\":null,\"fill_color\":null,\"parent_id\":null},\"gender\":\"F\",\"friends_count\":1,\"subscribers_count\":0,\"interests_count\":0,\"birth_year\":null,\"birth_month\":null,\"birth_day\":null,\"country\":null,\"region\":null,\"city\":null,\"map_image\":null,\"relation\":null,\"is_service\":false,\"last_act\":\"2015-08-19 10:41:28.473371\",\"is_online\":false}";
+    parseUser(inputJSON, users);
+
+    inputJSON = "{\"id\":1,\"name\":\"Dmitry\",\"age\":21,\"about\":null,\"avatar_id\":null,\"avatar\":{\"id\":null,\"url\":\"http:\\/\\/cdn.blabla.eu\\/img\\/sad_smile.png\",\"width\":null,\"height\":null,\"fill_color\":null,\"parent_id\":null},\"gender\":\"F\",\"friends_count\":1,\"subscribers_count\":0,\"interests_count\":0,\"birth_year\":null,\"birth_month\":null,\"birth_day\":null,\"country\":null,\"region\":null,\"city\":null,\"map_image\":null,\"relation\":null,\"is_service\":false,\"last_act\":\"2015-08-19 10:41:28.473371\",\"is_online\":false}";
+    parseUser(inputJSON, users);
+
+    for (auto it = users->begin(); it != users->end(); ++it)
+    {
+        std::cout << "User: " << endl
+            << (*it).second.id << endl
+            << (*it).second.name << endl
+            << (*it).second.age << endl;
+
+        cout << "====================" << endl << endl;
+    }
 
     return 0;
 }
